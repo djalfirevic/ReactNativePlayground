ccc6cfa2a0958c14f89acf7bc7597adeeb8a0032
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-08 09:48:02 +0200

    App split up into multiple components

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -5,39 +5,29 @@ import {
 	View,
 	TextInput,
 	Button,
-	ScrollView,
+	FlatList,
 } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
-	const [enteredGoal, setEnteredGoal] = useState('');
 	const [courseGoals, setCourseGoals] = useState([]);
 
-	const goalInputHandler = (enteredText) => {
-		setEnteredGoal(enteredText);
-	};
-
-	const addGoalHandler = () => {
-		setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+	const addGoalHandler = (goalTitle) => {
+		setCourseGoals((currentGoals) => [
+			...currentGoals,
+			{ id: Math.random().toString(), value: goalTitle },
+		]);
 	};
 
 	return (
 		<View style={styles.screen}>
-			<View style={styles.inputContainer}>
-				<TextInput
-					placeholder="Course Goal"
-					style={styles.input}
-					onChangeText={goalInputHandler}
-					value={enteredGoal}
-				/>
-				<Button title="ADD" onPress={addGoalHandler} />
-			</View>
-			<ScrollView>
-				{courseGoals.map((goal) => (
-					<View key={goal} style={styles.listItem}>
-						<Text>{goal}</Text>
-					</View>
-				))}
-			</ScrollView>
+			<GoalInput onAddGoal={addGoalHandler} />
+			<FlatList
+				keyExtractor={(item, index) => item.id}
+				data={courseGoals}
+				renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+			/>
 		</View>
 	);
 }
@@ -45,23 +35,5 @@ export default function App() {
 const styles = StyleSheet.create({
 	screen: {
 		padding: 50,
-	},
-	inputContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-	},
-	input: {
-		width: '80%',
-		borderColor: 'black',
-		borderWidth: 1,
-		padding: 10,
-	},
-	listItem: {
-		padding: 10,
-		marginVertical: 10,
-		backgroundColor: '#ccc',
-		borderColor: 'black',
-		borderWidth: 1,
 	},
 });
