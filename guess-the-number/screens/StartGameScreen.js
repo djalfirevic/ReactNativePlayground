0e629c82ffbed0da408d33b30704f7aacf9b2a5f
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-16 14:07:54 +0200

    Adding Card and Input components

--- guess-the-number/screens/StartGameScreen.js
+++ guess-the-number/screens/StartGameScreen.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
+import Input from '../components/Input';
+import Colors from '../constants/colors';
 
-const StartGameScreen = () => {
+const StartGameScreen = (props) => {
 	return (
 		<View style={styles.screen}>
 			<Text style={styles.title}>Start a New Game!</Text>
-			<View style={styles.inputContainer}>
+			<Card style={styles.inputContainer}>
 				<Text>Select a Number</Text>
-				<TextInput />
+				<Input
+					style={styles.input}
+					blurOnSubmit
+					autoCapitalize="none"
+					autoCorrect={false}
+					keyboardType="number-pad"
+					maxLength={2}
+					textAlign={'center'}
+				/>
 				<View style={styles.buttonContainer}>
-					<Button title="Reset" onPress={() => {}} />
-					<Button title="Confirm" onPress={() => {}} />
+					<View style={styles.button}>
+						<Button title="Reset" onPress={() => {}} color={Colors.accent} />
+					</View>
+					<View style={styles.button}>
+						<Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+					</View>
 				</View>
-			</View>
+			</Card>
 		</View>
 	);
 };
@@ -23,25 +38,26 @@ const styles = StyleSheet.create({
 		padding: 10,
 		alignItems: 'center',
 	},
-	title: { fontSize: 20, marginVertical: 10 },
+	title: {
+		fontSize: 20,
+		marginVertical: 10,
+	},
 	inputContainer: {
 		width: 300,
 		maxWidth: '80%',
 		alignItems: 'center',
-		shadowColor: 'black',
-		shadowOffset: { width: 0, height: 2 },
-		shadowOpacity: 0.26,
-		shadowRadius: 6,
-		backgroundColor: 'white',
-		elevation: 5,
-		padding: 20,
-		borderRadius: 10,
 	},
 	buttonContainer: {
 		flexDirection: 'row',
 		width: '100%',
 		justifyContent: 'space-between',
 		paddingHorizontal: 15,
+	},
+	button: {
+		width: 100,
+	},
+	input: {
+		width: 50,
 	},
 });
 
