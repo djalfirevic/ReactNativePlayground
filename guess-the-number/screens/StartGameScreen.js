4e9eafdbaa2d9946aff4268b9e9774477ccb6a3c
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-18 10:33:47 +0200

    GameScreen added

--- guess-the-number/screens/StartGameScreen.js
+++ guess-the-number/screens/StartGameScreen.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import Card from '../components/Card';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 import Colors from '../constants/colors';
 
 const StartGameScreen = (props) => {
@@ -46,12 +47,22 @@ const StartGameScreen = (props) => {
 		setConfirmed(true);
 		setSelectedNumber(chosenNumber);
 		setEnteredValue('');
+		Keyboard.dismiss();
 	};
 
 	let confirmedOutput;
 
 	if (confirmed) {
-		confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+		confirmedOutput = (
+			<Card style={styles.summaryContainer}>
+				<Text>You selected</Text>
+				<NumberContainer>{selectedNumber}</NumberContainer>
+				<Button
+					title="START GAME"
+					onPress={() => props.onStartGame(selectedNumber)}
+				/>
+			</Card>
+		);
 	}
 
 	return (
@@ -123,6 +134,10 @@ const styles = StyleSheet.create({
 	},
 	input: {
 		width: 50,
+	},
+	summaryContainer: {
+		marginTop: 20,
+		alignItems: 'center',
 	},
 });
 
