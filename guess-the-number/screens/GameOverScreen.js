6b921cfbd81d28c24dab0d286d90d9dea8a224bb
Djuro Alfirevic <djalfirevic@gmail.com>
2020-06-28 18:43:55 +0200

    Handling orientation changes on StartGameScreen

--- guess-the-number/screens/GameOverScreen.js
+++ guess-the-number/screens/GameOverScreen.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import {
+	View,
+	Text,
+	StyleSheet,
+	Dimensions,
+	Image,
+	ScrollView,
+} from 'react-native';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
 import Colors from '../constants/colors';
@@ -7,29 +14,31 @@ import MainButton from '../components/MainButton';
 
 const GameOverScreen = (props) => {
 	return (
-		<View style={styles.screen}>
-			<TitleText>The Game is Over!</TitleText>
-			<View style={styles.imageContainer}>
-				<Image
-					source={require('../assets/success.png')}
-					// source={{
-					// 	uri:
-					// 		'https://upload.wikimedia.org/wikipedia/commons/8/88/Summit_of_the_Matterhorn.jpg',
-					// }}
-					style={styles.image}
-					resizeMode="cover"
-				/>
+		<ScrollView>
+			<View style={styles.screen}>
+				<TitleText>The Game is Over!</TitleText>
+				<View style={styles.imageContainer}>
+					<Image
+						source={require('../assets/success.png')}
+						// source={{
+						// 	uri:
+						// 		'https://upload.wikimedia.org/wikipedia/commons/8/88/Summit_of_the_Matterhorn.jpg',
+						// }}
+						style={styles.image}
+						resizeMode="cover"
+					/>
+				</View>
+				<View style={styles.resultContainer}>
+					<BodyText style={styles.resultText}>
+						Your phone needed{' '}
+						<Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+						guess the number{' '}
+						<Text style={styles.highlight}>{props.userNumber}</Text>.
+					</BodyText>
+				</View>
+				<MainButton onPress={props.onRestart}>NEW GAME</MainButton>
 			</View>
-			<View style={styles.resultContainer}>
-				<BodyText style={styles.resultText}>
-					Your phone needed{' '}
-					<Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-					guess the number{' '}
-					<Text style={styles.highlight}>{props.userNumber}</Text>.
-				</BodyText>
-			</View>
-			<MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-		</View>
+		</ScrollView>
 	);
 };
 
@@ -40,13 +49,13 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 	},
 	imageContainer: {
-		width: 300,
-		height: 300,
-		borderRadius: 150,
+		width: Dimensions.get('window').width * 0.7,
+		height: Dimensions.get('window').width * 0.7,
+		borderRadius: (Dimensions.get('window').width * 0.7) / 2,
 		borderWidth: 3,
 		borderColor: 'black',
 		overflow: 'hidden',
-		marginVertical: 30,
+		marginVertical: Dimensions.get('window').height / 20,
 	},
 	image: {
 		width: '100%',
@@ -54,11 +63,11 @@ const styles = StyleSheet.create({
 	},
 	resultContainer: {
 		marginHorizontal: 30,
-		marginVertical: 15,
+		marginVertical: Dimensions.get('window').height / 40,
 	},
 	resultText: {
 		textAlign: 'center',
-		fontSize: 20,
+		fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
 	},
 	highlight: {
 		color: Colors.primary,
--- guess-the-number/screens/StartGameScreen.js
+++ guess-the-number/screens/StartGameScreen.js
@@ -1,51 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
 	View,
+	Text,
 	StyleSheet,
 	Button,
 	TouchableWithoutFeedback,
 	Keyboard,
 	Alert,
+	Dimensions,
+	ScrollView,
+	KeyboardAvoidingView,
 } from 'react-native';
+
 import Card from '../components/Card';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
-import Colors from '../constants/colors';
 import MainButton from '../components/MainButton';
+import Colors from '../constants/colors';
 
 const StartGameScreen = (props) => {
 	const [enteredValue, setEnteredValue] = useState('');
 	const [confirmed, setConfirmed] = useState(false);
 	const [selectedNumber, setSelectedNumber] = useState();
+	const [buttonWidth, setButtonWidth] = useState(
+		Dimensions.get('window').width / 4
+	);
 
 	const numberInputHandler = (inputText) => {
-		setEnteredValue(inputText.replace(/[^0-9]/g), '');
+		setEnteredValue(inputText.replace(/[^0-9]/g, ''));
 	};
 
-	const resetInputHandler = (inputText) => {
+	const resetInputHandler = () => {
 		setEnteredValue('');
 		setConfirmed(false);
 	};
 
-	const confirmInputHandler = (inputText) => {
+	useEffect(() => {
+		const updateLayout = () => {
+			setButtonWidth(Dimensions.get('window').width / 4);
+		};
+
+		Dimensions.addEventListener('change', updateLayout);
+		return () => {
+			Dimensions.removeEventListener('change', updateLayout);
+		};
+	});
+
+	const confirmInputHandler = () => {
 		const chosenNumber = parseInt(enteredValue);
 		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
 			Alert.alert(
-				'Invalid number',
+				'Invalid number!',
 				'Number has to be a number between 1 and 99.',
-				[
-					{
-						text: 'OK',
-						style: 'destructive',
-						onPress: resetInputHandler,
-					},
-				]
+				[{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
 			);
 			return;
 		}
-
 		setConfirmed(true);
 		setSelectedNumber(chosenNumber);
 		setEnteredValue('');
@@ -67,45 +79,48 @@ const StartGameScreen = (props) => {
 	}
 
 	return (
-		<TouchableWithoutFeedback
-			onPress={() => {
-				Keyboard.dismiss();
-			}}>
-			<View style={styles.screen}>
-				<TitleText style={styles.title}>Start a New Game!</TitleText>
-				<Card style={styles.inputContainer}>
-					<BodyText>Select a Number</BodyText>
-					<Input
-						style={styles.input}
-						blurOnSubmit
-						autoCapitalize="none"
-						autoCorrect={false}
-						keyboardType="number-pad"
-						maxLength={2}
-						textAlign={'center'}
-						onChangeText={numberInputHandler}
-						value={enteredValue}
-					/>
-					<View style={styles.buttonContainer}>
-						<View style={styles.button}>
-							<Button
-								title="Reset"
-								onPress={resetInputHandler}
-								color={Colors.accent}
+		<ScrollView>
+			<KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30}>
+				<TouchableWithoutFeedback
+					onPress={() => {
+						Keyboard.dismiss();
+					}}>
+					<View style={styles.screen}>
+						<TitleText style={styles.title}>Start a New Game!</TitleText>
+						<Card style={styles.inputContainer}>
+							<BodyText>Select a Number</BodyText>
+							<Input
+								style={styles.input}
+								blurOnSubmit
+								autoCapitalize="none"
+								autoCorrect={false}
+								keyboardType="number-pad"
+								maxLength={2}
+								onChangeText={numberInputHandler}
+								value={enteredValue}
 							/>
-						</View>
-						<View style={styles.button}>
-							<Button
-								title="Confirm"
-								onPress={confirmInputHandler}
-								color={Colors.primary}
-							/>
-						</View>
+							<View style={styles.buttonContainer}>
+								<View style={{ width: buttonWidth }}>
+									<Button
+										title="Reset"
+										onPress={resetInputHandler}
+										color={Colors.accent}
+									/>
+								</View>
+								<View style={{ width: buttonWidth }}>
+									<Button
+										title="Confirm"
+										onPress={confirmInputHandler}
+										color={Colors.primary}
+									/>
+								</View>
+							</View>
+						</Card>
+						{confirmedOutput}
 					</View>
-				</Card>
-				{confirmedOutput}
-			</View>
-		</TouchableWithoutFeedback>
+				</TouchableWithoutFeedback>
+			</KeyboardAvoidingView>
+		</ScrollView>
 	);
 };
 
@@ -122,6 +137,7 @@ const styles = StyleSheet.create({
 	},
 	inputContainer: {
 		width: '80%',
+		// maxWidth: '80%',
 		maxWidth: '95%',
 		minWidth: 300,
 		alignItems: 'center',
@@ -132,11 +148,13 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between',
 		paddingHorizontal: 15,
 	},
-	button: {
-		width: 100,
-	},
+	// button: {
+	//   // width: 100
+	//   width: Dimensions.get('window').width / 4
+	// },
 	input: {
 		width: 50,
+		textAlign: 'center',
 	},
 	summaryContainer: {
 		marginTop: 20,
